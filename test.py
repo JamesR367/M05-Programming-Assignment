0cd9efda0860a90250d515e54eb0de6f0f38236d
James <jarjrjarsr@gmail.com>
2023-04-24 23:10:55 -0400

    Initial commit

---
+++ test.py
@@ -0,0 +1,28 @@
+import unittest
+
+from my_sum import sum
+from fractions import Fraction
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+if __name__ == '__main__':
+    unittest.main()
+
+# The test make sure that the code runs with no bugs and that all the syntax is correct. If there is a problem in the code the test will point to where the problem is 
+# and possible fixes for it.
